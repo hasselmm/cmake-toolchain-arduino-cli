3bd0da706f1e22e4d6f39e0e9193c1f506ad34ec
Mathias Hasselmann <mathias@taschenorakel.de>
2024-08-16 00:06:20 +0200

    Add a another shell-quote hack for STM32

--- toolchain/arduino-cli-toolchain.cmake
+++ toolchain/arduino-cli-toolchain.cmake
@@ -382,17 +382,21 @@ endfunction()
 # Converts a shell quoted command string into a COMMAND list for execute_process().
 # ----------------------------------------------------------------------------------------------------------------------
 function(__arduino_make_command_list SHELL_QUOTED OUTPUT_VARIABLE)
-    set(_quoted   "\"([^\"]*)\"")   # quoted command line argument
-    set(_literal  "([^\"\t ]+)")    # literal command line argument without spaces or quotes
-    set(_tail     "[ \t]+(.*)|$")   # the next argument separating space and the remaining text
-    set(_argument "${_quoted}|${_literal}")
+    set(_quoted   "\"([^\"]*)\"")               # quoted command line argument
+    set(_exquoted "([\\]\"[^\"]*[\\]\")")       # explicitly quoted
+    set(_literal  "([^\"\t ]+)")                # literal command line argument without spaces or quotes
+    set(_tail     "[ \t]+(.*)|$")               # the next argument separating space and the remaining text
+    set(_argument "${_quoted}|${_exquoted}|${_literal}")
 
     set(_command "${SHELL_QUOTED}")
     set(_command_list)
 
+    # This SHELL-QUOTE HACK is needed for STM32 builds.
+    string(REPLACE "\"\"" "\\\"" _command "${_command}")
+
     while (_command AND _command MATCHES "^(${_argument})(${_tail})") # <------------------------ split the command line
-        list(APPEND _command_list "${CMAKE_MATCH_2}${CMAKE_MATCH_3}") # one of them
-        set(_command "${CMAKE_MATCH_5}")
+        list(APPEND _command_list "${CMAKE_MATCH_2}${CMAKE_MATCH_3}${CMAKE_MATCH_4}") # one of them
+        set(_command "${CMAKE_MATCH_6}")
     endwhile()
 
     if (_command)
