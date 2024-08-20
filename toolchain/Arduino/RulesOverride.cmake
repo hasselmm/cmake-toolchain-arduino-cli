cb853607965a38986e9bbc6dae502396bc4b69eb
Mathias Hasselmann <mathias@taschenorakel.de>
2024-08-20 22:46:05 +0200

    Remove redudant comment

--- toolchain/Arduino/RulesOverride.cmake
+++ toolchain/Arduino/RulesOverride.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_ASM_OUTPUT_EXTENSION ".o") # <---------------------- align object file extensions with Arduino for convenience
+set(CMAKE_ASM_OUTPUT_EXTENSION ".o")
 set(CMAKE_C_OUTPUT_EXTENSION   ".o")
 set(CMAKE_CXX_OUTPUT_EXTENSION ".o")
 
